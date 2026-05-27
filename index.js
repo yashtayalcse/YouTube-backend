0b527e1d95fb742e836d7aac03a0c1ac0e78e5e7
yashtayalcse <yashtayal28763@gmail.com>
2026-05-27 18:17:58 +0530

    added middlewares

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 import "dotenv/config";
 import connectDB from "./src/db/index.js";
+import app from "./app.js";
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.on("error",(error)=>{ //register this listener
+    console.log("ERROR HAPPENDED FROM index.js app object",error);
+  })
+
+  app.listen(process.env.PORT,()=>{
+    console.log("Server running on port: ",process.env.PORT)
+  })
+})
+.catch((error)=>{
+  console.log("MONGODB CONNECTION FAILED FROM index.js: ",error)
+})
--- src/db/index.js
+++ src/db/index.js
@@ -1,25 +1,14 @@
 import mongoose from "mongoose";
-import express from "express";
 import { DB_NAME } from "../../constants.js";
-
-const app=express();
 
 
 const connectDB = async ()=>{
   try {
     let connection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     console.log("Connected to database: ", connection.connections[0].host);
-
-    app.on("error",(error)=>{ //register this listener
-      console.log("ERROR HAPPENED FROM db/index.js file: ",error);
-    })
-
-    app.listen(process.env.PORT,()=>{
-      console.log("Server running on port: ",process.env.PORT)
-    })
-
   } catch (error) {
-    console.error("ERROR connecting to DB: ",error);
+    console.log("ERROR connecting to DB: ",error);
+    throw error;
   }
 }
 
