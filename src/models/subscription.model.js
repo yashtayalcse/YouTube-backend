84e2ac1f6f2b285932e299b3ceca6534e53ce60c
yashtayalcse <yashtayal28763@gmail.com>
2026-06-05 19:42:58 +0530

    add scubscription schema

---
+++ src/models/subscription.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const SubscriptionSchema = new mongoose.Schema({
+  subscriber: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  channel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+})
+
+export const Subscription = mongoose.model("Subscription",SubscriptionSchema); 
